59633f7ee5f377adaefe6cbd46d29a8aecd04f98
Kevin Thomas <kevin@mytechnotalent.com>
2025-08-02 15:28:35 -0400

    Added chapters 3-5

--- code/0x01_RCC-clock/src/main.c
+++ code/0x01_RCC-clock/src/main.c
@@ -2,7 +2,7 @@
 
 int main(void) {
     // Initialize the AHB1 system clock for GPIOA
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // RM p.118
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // RM p.118 
 
     // Main loop
     while (1) {
